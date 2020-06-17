c823e8c64899d2b2352f972c1ae09a0a2c27f35c
Jameson Batista <jameson.b.araujo@accenture.com>
2020-06-17 00:09:58 -0300

    update login register animation

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import * as React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {DrawerNavigator} from './Components/DrawerContent';
+import {LogintStack} from './Components/Stacks';
 
-export default function App() {
+export default function App({user}) {
   return (
     <NavigationContainer>
-      <DrawerNavigator />
+      {!user ? <LogintStack /> : <DrawerNavigator />}
     </NavigationContainer>
   );
 }
